e05247af8cd602608aaf548beab27b044b29dbb2
wsilvadev <wsilvadev@gmail.com>
2024-08-03 13:18:57 -0300

    chore: init project and configure prettier

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+/**
+ * .eslint.js
+ *
+ * ESLint configuration file.
+ */
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'vuetify',
+    '@vue/eslint-config-typescript',
+    './.eslintrc-auto-import.json',
+  ],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+};
---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <Avatar />
+</template>
+
+<script lang="ts" setup>
+//
+</script>

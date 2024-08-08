8dcb80683ea9a53895ddcb063f16feccbb2b5fb4
wsilvadev <wsilvadev@gmail.com>
2024-08-08 20:16:12 -0300

    feat: implement avatar left wrapper

--- src/views/home/header.vue
+++ src/views/home/header.vue
@@ -1,21 +1,157 @@
 <template>
   <div class="container-header">
-    <div>Works</div>
-    <div>Blog</div>
-    <div>Contact</div>
+    <div class="__name">
+      <p>will</p>
+      <p>dev</p>
+    </div>
+    <div class="__options">
+      <div class="container__menu-items">
+        <div class="menu-items" v-for="item in itemsMenu" :key="item">
+          <v-btn
+            :append-icon="item?.icon"
+            @click="redirectTo(item.page, item?.isOutsidePage)"
+          >
+            {{ item.text }}
+          </v-btn>
+        </div>
+      </div>
+
+      <div>
+        <v-btn @click="redirectTo('/', false)">
+          <v-icon icon="mdi-github" />
+        </v-btn>
+        <v-btn  @click="redirectTo('/', false)" >
+          <v-icon icon="mdi-linkedin" />
+        </v-btn>
+      </div>
+
+      <v-overlay :model-value="overlay" class="align-center justify-center">
+        <Avatar />
+      </v-overlay>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data: () => ({
+    overlay: false,
+    itemsMenu: [
+      {
+        text: 'Blog',
+        page: '/',
+      },
+      {
+        text: 'FAQ',
+        page: '',
+      },
+      {
+        text: 'Work',
+        page: 'https://www.qualicorp.com.br/',
+        isOutsidePage: true,
+        icon: 'mdi-menu-right',
+      },
+      {
+        text: 'Services',
+        page: '',
+      },
+      {
+        text: 'About',
+        page: '',
+      },
+      {
+        text: 'Contact',
+        page: '',
+      },
+    ],
+  }),
+  methods: {
+    redirectTo(page, outside_url) {
+      this.overlay = !this.overlay;
+      setTimeout(() => {
+        this.overlay = false;
+        if (outside_url) window.open(page, '_blank');
+        else {
+          this.$router.push(page);
+        }
+      }, 4000);
+    },
+  },
+};
+</script>
 <style lang="scss" scoped>
 .container-header {
   display: flex;
-  width: 100%;
-  justify-content: flex-end;
   gap: 20px;
-  height: 40px;
-  align-items: center;
-  padding-right: 10px;
-  div {
-    font-weight: bold;
+  width: 100%;
+  padding: 30px;
+
+  .__name {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    p {
+      font-weight: bold;
+      font-size: 24px;
+    }
+
+    p:nth-child(2) {
+      color: var(--color-purple);
+    }
+  }
+
+  .__options {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+
+    .v-btn {
+      background: transparent;
+      border: none;
+      box-shadow: none;
+      font-size: 14px;
+      border-radius: 30px;
+      text-transform: none;
+      color: var(--color-purple);
+    }
+    .container__menu-items {
+      display: flex;
+      flex-direction: row;
+    }
+
+    .menu-items:nth-child(1),
+    :nth-child(2) {
+      .v-btn {
+        color: var(--color-blue);
+      }
+    }
+
+    .menu-items:nth-child(3) {
+      .v-btn {
+        font-size: 16px;
+        background-color: var(--background-color-button-purple);
+      }
+    }
+
+    .menu-items:nth-child(4)::after {
+      content: '';
+      display: inline-block;
+      background-color: var(--color-purple);
+      width: 3px;
+      height: 3px;
+      border-radius: 50%;
+    }
+
+    .menu-items:nth-child(5)::after {
+      content: '';
+      display: inline-block;
+      background-color: var(--color-purple);
+      width: 3px;
+      height: 3px;
+      border-radius: 50%;
+    }
   }
 }
 </style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,27 +2,230 @@
 <template>
   <div>
     <Header />
-    <div class="container-profile">
-      <div>left</div>
-      <div class="container-avatar">
-        <Avatar />
+    <div class="container-wrapper">
+      <div class="__description">
+        <div class="__title">
+          <p>I’m
+          <p>Willian</p> Simões.</p>
+          <span>creative and <b>technical</b> frontend <b>developer</b>.</span>
+        </div>
+        <div class="__stacks">
+          <p>tech stack / tools favorit saat ini:</p>
+          <div class="__stacks__item">
+            <v-icon icon="mdi-language-typescript" />
+            <v-icon icon="mdi-vuejs" />
+            <v-img src="../../assets/stack/pwa-icon.webp" />
+            <v-icon icon="mdi-git" />
+            <div class="icon-react-native">
+              <v-icon icon="mdi-react" />
+              React Native
+            </div>
+            <v-icon icon="mdi-android" />
+            <v-icon icon="mdi-apple-ios" />
+          </div>
+        </div>
+
+      </div>
+      <div class="container__avatar">
+        <div class="__avatar">
+          <v-img src="../../assets/avatar.png" />
+        </div>
+      </div>
+    </div>
+    <div class="container-jobs">
+      <div class="container__marketing">
+        <div class="__marketing">
+          <v-img src="../../assets/marketing/clean.png" />
+          <div class="__marketing__title">
+            Clean & Modern Style
+          </div>
+          <div class="__marketing__description">
+            Senang membuat design yang modern, clean serta UI yang intuitif.
+          </div>
+        </div>
+        <div class="__marketing">
+          <v-img src="../../assets/marketing/heart.png" />
+          <div class="__marketing__title">
+            Detail Oriented
+          </div>
+          <div class="__marketing__description">
+            Tantangan terbesarnya: aksesibilitas, browser kompatibilitas dan desain responsive.
+          </div>
+        </div>
+        <div class="__marketing">
+          <v-img src="../../assets/marketing/optimized.png" />
+          <div class="__marketing__title">
+            Fast & Optimized
+          </div>
+          <div class="__marketing__description">
+            Penulisan code yang terstruktur, optimal serta efisien.
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <style scoped>
-.container-profile {
+.container-wrapper {
   display: flex;
   justify-content: space-between;
-  padding: 0px 20px 0px 20px;
-  width: 100vw;
-  height: 100vh;
+  height: 60vh;
+  background-color: var(--background-color-wrapper);
 
-  background: red;
+  .__description {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    padding: 10% 0 0 20%;
+    flex-direction: column;
+
+    .__title {
+      display: flex;
+      flex-direction: column;
+
+      p {
+        display: inline;
+        font-size: 50px;
+        font-family: var(--font-poppins);
+        font-weight: 900;
+        color: var(--color-silver-72-opacity);
+      }
+
+      p>p {
+        color: var(--color-purple);
+      }
+
+      span {
+        font-weight: 300;
+      }
+    }
+
+    .__stacks {
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 20%;
+
+      p {
+        color: var(--color-silver-50-opacity);
+        font-weight: 500;
+      }
+
+      .__stacks__item {
+        display: flex;
+        flex-direction: row;
+        width: 200px;
+        color: var(--color-silver);
+
+        .v-img {
+          width: 20px;
+          height: 20px;
+          padding: 0;
+        }
+
+        .icon-react-native {
+          .v-icon {
+            font-size: 25px;
+          }
+
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          font-size: 7px;
+        }
+      }
+    }
+  }
+
+  .container__avatar {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: 80%;
+    background-color: var(--background-color-wrapper);
+
+    .__avatar {
+      display: flex;
+      min-width: 389px;
+      height: 90%;
+      position: relative;
+      border-radius: 0 0 45% 45%;
+
+      .v-img {
+        bottom: 0;
+        z-index: 1;
+        overflow: visible;
+        background-color: var(--color-purple);
+        max-width: 389px;
+      }
+
+      ::v-deep .v-img__img--contain {
+        object-fit: cover;
+      }
+
+      ::v-deep .v-img__img {
+        left: -90px;
+        z-index: 9999;
+        top: 20px;
+        transform: rotate(-10deg);
+      }
+    }
+
+    .__avatar::before {
+      content: '';
+      width: 100%;
+      height: 70%;
+      bottom: 0;
+      position: absolute;
+      background-color: transparent;
+      border-radius: 0 0 45% 45%;
+      box-shadow: 38px 200px 0px 38px var(--background-color-wrapper);
+      z-index: 2;
+    }
+  }
 }
-.container-avatar {
-  width: 200px;
-  margin: 30px 30px 0px 0px;
-  background-color: blue;
+
+.container-jobs {
+  position: relative;
+  background-color: #fff;
+  width: 100%;
+  height: 100vh;
+  z-index: 3;
+
+  .container__marketing {
+    display: flex;
+    justify-content: stretch;
+    position: absolute;
+    top: -60px;
+    left: 20%;
+    width: 67%;
+    height: 198px;
+    gap: 40px;
+
+    .__marketing {
+      width: 100%;
+      height: 100%;
+      background-color: #fff;
+      justify-content: flex-start;
+      padding: 40px;
+      border: 1px solid var(--color-silver-04-opacity);
+
+      .v-img {
+        width: 50px;
+      }
+
+      .__marketing__title {
+        font-size: 18px;
+        font-weight: 700;
+        color: var(--color-silver-92-opacity);
+        margin-top: 10px;
+        margin-bottom: 5px;
+      }
+      .__marketing__description {
+        font-size: 14px;
+        font-weight: 200;
+        color: var( --color-silver-82-opacity);
+      }
+    }
+  }
 }
 </style>
